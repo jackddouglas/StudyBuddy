1d04e26962ecf18a73b4d38514334c715c125887
Jack D. Douglas <jackdouglas2@me.com>
2018-07-31 12:40:41 -0700

    added initial ui and created most base code documents

---
+++ StudyBuddy/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  StudyBuddy
+//
+//  Created by Jack Douglas on 7/26/18.
+//  Copyright © 2018 Jack D. Douglas. All rights reserved.
+//
+//  This is the model class that represents the blueprint for a message
+//
+
+class Message {
+
+    //TODO: Messages need a messageBody and a sender variable
+
+
+
+}
